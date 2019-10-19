d5c3632a9b72124f2c65e8ac9cbde141ef8b5491
gabriel_hijo <gabriel_hijo@gabriel_hijo-PC.home>
2019-10-19 20:05:45 +0200

    Implements the attributes, build and getters of the Currency Class.

--- src/moneycalculator/Currency.java
+++ src/moneycalculator/Currency.java
@@ -1,5 +1,26 @@
 package moneycalculator;
 
 public class Currency {
+    private String code;
+    private String name;
+    private String symbol;
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public Currency(String code, String name, String symbol) {
+        this.code = code;
+        this.name = name;
+        this.symbol = symbol;
+    }
+    
     
 }
