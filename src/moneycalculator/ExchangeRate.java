b6a4383fe0124d894103de7ca8f0da5ae5e7c969
gabriel_hijo <gabriel_hijo@gabriel_hijo-PC.home>
2019-10-19 20:03:51 +0200

    Implements the attributes, build and getters of the ExchangeRate Class.

--- src/moneycalculator/ExchangeRate.java
+++ src/moneycalculator/ExchangeRate.java
@@ -1,5 +1,22 @@
 package moneycalculator;
 
+import java.util.Date;
+
 public class ExchangeRate {
+    private Date date ;
+    private double rate;
+
+    public ExchangeRate(Date date, double rate) {
+        this.date = date;
+        this.rate = rate;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public double getRate() {
+        return rate;
+    }
     
 }
